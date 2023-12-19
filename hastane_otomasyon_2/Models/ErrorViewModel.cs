3e09f5362d739ac9d5c2cae4d5477dd7fc836a4c
feyzaocak <feyzaocak@icloud.com>
2023-12-19 14:47:22 +0300

    Add project files.

---
+++ hastane_otomasyon_2/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace hastane_otomasyon_2.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
