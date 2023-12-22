2f1079d1d46dffb299fa412677b64b768a3c6829
feyzaocak <feyzaocak@icloud.com>
2023-12-22 23:44:52 +0300

    Doktorlarımız sayfası eklendi

---
+++ hastane_otomasyon_2/Models/Doktor.cs
@@ -0,0 +1,18 @@
+﻿
+using System.ComponentModel.DataAnnotations;
+
+namespace hastane_otomasyon_2.Models
+{
+    public class Doktor
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string AdSoyad { get; set; }
+
+        [Required]
+        public string UzmanlikAlani { get; set; }
+
+       
+    }
+}
