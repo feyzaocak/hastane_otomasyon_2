f4ec6d40023952deb6b3c1a04ef568010be8dfe5
feyzaocak <feyzaocak@icloud.com>
2023-12-29 23:32:11 +0300

    Tibbi Birim sayfa düzenlemesi

--- hastane_otomasyon_2/Controllers/TibbiBirimlerController.cs
+++ hastane_otomasyon_2/Controllers/TibbiBirimlerController.cs
@@ -1,12 +1,20 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using hastane_otomasyon_2.Models;
+using System.Collections.Generic;
 
-namespace hastane_otomasyon_2.Controllers
+public class TibbiBirimlerController : Controller
 {
-    public class TibbiBirimlerController : Controller
+    public IActionResult Index()
     {
-        public IActionResult Index()
+        // Örnek tıbbi birim verileri
+        var tibbiBirimListesi = new List<TibbiBirimModel>
         {
-            return View();
-        }
+            new TibbiBirimModel { Id = 1, BirimAdi = "Dahiliye", Aciklama = "İç hastalıklarıyla ilgilenen birim" },
+            new TibbiBirimModel { Id = 2, BirimAdi = "Cerrahi", Aciklama = "Ameliyatlarla ilgilenen birim" },
+            new TibbiBirimModel { Id = 3, BirimAdi = "Göz Hastalıkları", Aciklama = "Göz hastalıklarıyla ilgilenen birim" }
+            // ... diğer birimler
+        };
+
+        return View(tibbiBirimListesi);
     }
 }
---
+++ hastane_otomasyon_2/Models/TibbiBirimModel.cs
@@ -0,0 +1,9 @@
+﻿namespace hastane_otomasyon_2.Models
+{
+    public class TibbiBirimModel
+    {
+        public int Id { get; set; }
+        public string BirimAdi { get; set; }
+        public string Aciklama { get; set; }
+    }
+}
